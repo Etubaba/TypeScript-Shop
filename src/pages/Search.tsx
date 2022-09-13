7a0726188b89b90bd504f26bfcc59f6889cba3f0
Etumnu Prosper <johnikechukwu94@gmail.com>
2022-09-13 07:54:38 +0100

    update search page

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -8,6 +8,8 @@ const Search = () => {
 
     const [products,setProducts]=useState<Iproduct[]>([])
 
+    const [search,setSearch]=useState<string>('')
+
 
 interface Iproduct {
   _id: string
@@ -32,13 +34,23 @@ interface Iproduct {
    
   }, [])
 
+
+  const filteredProducts=products.filter(product =>{
+    if(search===''){
+      return product
+    }else if(product.productName.toLocaleLowerCase()
+    .includes(search.toLocaleLowerCase())){
+      return product;
+    }
+  })
+
      return (
         <div>
         <div className='mx-auto max-w-5xl mt-10 w-[90%]  md:w-[60%] '>
-            <input type='text' className='w-full z-50  rounded-full p-4 outline-none text-white bg-[#2B3039]' placeholder='Search for a Product here   '/>
+            <input onChange={(e)=>setSearch(e.target.value)} value={search} type='text' className='w-full z-50  rounded-full p-4 outline-none text-white bg-[#2B3039]' placeholder='Search for a Product here   '/>
         </div>
       <div className="grid z-50 gap-4 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 mx-5 md:mx-auto mt-24  md:max-w-5xl">
-      {products.map((item)=>
+      {filteredProducts.map((item)=>
       <div>
         <Card {...item} />
       </div>)}
